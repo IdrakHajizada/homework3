7cdb588d3d6d1bc89cac1d991774af76f37f866f
IdrakHajizada <94737790+IdrakHajizada@users.noreply.github.com>
2022-08-15 00:11:47 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Task No 3
+ function Start(){
+   let TestNum = prompt('Eded daxil edin');
+   let SecNum = TestNum % 2
+
+    if(SecNum = 0){
+        alert('Cut ededdir')
+    }
+    else{
+        alert('tek ededdir')
+    }
+ }
+
+
+
+
